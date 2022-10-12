5313faba470cda9c579573703070a2221e8ce134
Ojwang1 <wayanicholas@gmail.com>
2022-10-12 20:25:21 +0300

    initial commit

---
+++ database_and_tables_file.py
@@ -0,0 +1,25 @@
+from peewee import *
+from os import path
+# this is how to specify the path where the db is to be stored
+db_connection_path = path.dirname(path.realpath(__file__))
+# this is how to create our DB
+db = SqliteDatabase(path.join(db_connection_path, "emobilis.db"))
+# start creating tables
+class User(Model):
+    name = CharField()
+    email = CharField(unique=True)
+    password = CharField()
+    # this is how to put our table into our database
+    class Meta:
+        database = db
+
+class Product(Model):
+    name = CharField()
+    quantity = DoubleField()
+    price = DoubleField
+    class Meta:
+        database = db
+
+# Finally create the two tables
+User.create_table(fail_silently=True)
+Product.create_table(fail_silently= True)
